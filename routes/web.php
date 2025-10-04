63d37da0b3b95f9e9c999e2ff8608766e6aed16a
Nenad Zivanovic <office@permalink.rs>
2025-10-04 06:28:15 +0200

    wip

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,15 @@
 use App\Http\Controllers\TransactionsCotrnoller;
 use Illuminate\Support\Facades\Route;
 
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    return Inertia::render('Welcome');
+})->name('home');
+
+Route::get('dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     // Transactions pages
